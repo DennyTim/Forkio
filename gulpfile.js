89c7f2195c02daf76cc9187acf9ed0607859704d
Tim <luckystudydanit@gmail.com>
2018-10-12 19:35:21 +0300

    12/10/18 Step_Forkio -  Finished gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,62 +3,33 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var clean = require('gulp-clean');
+var sourcemaps = require('gulp-sourcemaps');
+var minify = require('gulp-uglify');
 
-//var minify = require('gulp-uglify');
-//var clean = require('gulp-clean');
-//var concat = require('gulp-concat');
-//var imagemin = require('gulp-imagemin');
+gulp.task('clean-build', function(){
+	return gulp.src('./build', {read: false})
+		.pipe(clean())
+})
 
-//gulp.task('hello', function(){
-//    console.log('Hello world')
-//})
-//
-//gulp.task('minify', function(){
-//    return gulp.src('./src/js/**/*.js')
-//        .pipe(minify())
-//        .pipe(gulp.dest('./build'))
-//})
-//
-//gulp.task('allinone', function () {
-//    return gulp.src('./src/js/**/*.js')
-//        .pipe(concat('bundle.js'))
-//        .pipe(gulp.dest('./build'))
-//})
+gulp.task('sass', ['clean-build'], function(){
+    return gulp.src('./src/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./build/css'))
+})
 
-gulp.task('serve',  function (){
+gulp.task('serve', ['sass'], function (){
     browserSync.init({
         server: "./"
     })
 
-    //gulp.watch('./src/js/**/*.js',['minify']);
-    gulp.watch('./src/scss/**/*.scss',['sass']);
-    gulp.watch('./index.html').on('change', browserSync.reload)
+    //gulp.watch('./src/css/**/*.css', ['minify']);
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch("./index.html").on('change', browserSync.reload);
 })
 
-gulp.task('sass', function(){
-    return gulp.src('./src/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./build/css'))
-})
-
-//gulp.task('clean', function () {
-//    return gulp.src('./build', {read: false})
-//        .pipe(clean())
-//})
-
-//gulp.task('img', function(){
-//    return gulp.src('./src/img/*')
-//        .pipe(imagemin({
-//                interlaced: true,
-//                progressive: true,
-//                svgoPlugins: [{removeViewBox:false}],
-//            })
-//        )
-//        .pipe(gulp.dest('./build/img'))
-//});
-
-
-
-gulp.task('default', ['serve', 'sass'], function(){
+gulp.task('default', ['serve'], function(){
     console.log('=== ALL DONE ===')
 })
