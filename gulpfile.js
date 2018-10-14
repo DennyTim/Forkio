8abbb79ccc304f0c57ff61b230819f5b5c24bbbe
Valentyna Lysenok <tina.lysenok@gmail.com>
2018-10-14 20:57:01 +0300

    refreshed gulpfile.js with image uploading functionality in build folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,30 +6,44 @@ var browserSync = require('browser-sync').create();
 var clean = require('gulp-clean');
 var sourcemaps = require('gulp-sourcemaps');
 //var minify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('clean-build', function(){
 	return gulp.src('./build', {read: false})
 		.pipe(clean())
-})
+});
 
-gulp.task('sass', ['clean-build'], function(){
+gulp.task('img', ['clean-build'], function(){
+    return gulp.src('./src/img/*.png')
+        .pipe(imagemin({
+                interlaced: true,
+                progressive: true,
+                svgoPlugins: [{removeViewBox:false}],
+            })
+        )
+        .pipe(gulp.dest('./build/img'))
+});
+
+gulp.task('sass', ['img'], function(){
     return gulp.src('./src/scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./build/css'))
-})
+});
 
 gulp.task('serve', ['sass'], function (){
     browserSync.init({
         server: "./"
-    })
+    });
 
     //gulp.watch('./src/css/**/*.css', ['minify']);
     gulp.watch('src/scss/**/*.scss', ['sass']);
     gulp.watch("./index.html").on('change', browserSync.reload);
-})
+});
 
 gulp.task('default', ['serve'], function(){
     console.log('=== ALL DONE ===')
-})
+});
+
+
