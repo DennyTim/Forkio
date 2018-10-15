6044e65c30d95ebaac073def0fe290c65d42451e
Valentyna Lysenok <tina.lysenok@gmail.com>
2018-10-15 21:08:26 +0300

    updated pricing-plans visability in subscribe section

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,26 @@
+/*----------S U B S C R I P T I O N   S E C T I O N-----------*/
 $(function ourServices() {
 
     $("#plans").on("click", ".subscription-plan", function () {
 
         let tabs = $("#plans .subscription-plan"),
-            cont = $("#plans .subscription-plan-header");
+            cont = $("#plans .subscription-plan-header"),
+            btn = $("#plans .plan-btn-purchase"),
+            cond = $("#plans .subscription-plan-conditions");
 
         // Удаляем классы active
         tabs.removeClass("active");
         cont.removeClass("active-header");
+        btn.removeClass("active-plan-btn");
+        cond.removeClass("active-conditions");
+
+
         // Добавляем классы active
         $(this).addClass("active");
         cont.eq($(this).index()).addClass("active-header");
+        btn.eq($(this).index()).addClass("active-plan-btn");
+        cond.eq($(this).index()).addClass("active-conditions");
         return false;
     });
 });
+/*---------------------------------------------*/
