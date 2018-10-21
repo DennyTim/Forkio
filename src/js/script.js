c7dec35d4444280006b19798d5b386d6ec21f77b
Tim <luckystudydanit@gmail.com>
2018-10-21 17:34:31 +0300

    21/10/2018 Finished add changes by methodology BEM.

--- src/js/script.js
+++ src/js/script.js
@@ -2,19 +2,19 @@ function slider (slides) {
 	let count = 0;
 	let arr = [...slides];
 	let clear = function() {
-		arr.map(function(element) {element.classList.remove("active")})
+		arr.map(function(element) {element.classList.remove("carousel__item_active")})
 	}
 	this.next = function () {
 		++count;
 		if (count >= arr.length) count = 0;
 		clear();
-		arr[count].classList.add("active");
+		arr[count].classList.add("carousel__item_active");
 	}
 	this.prev = function () {
 		--count;
 		if (count < 0) count = arr.length-1;
 		clear();
-		arr[count].classList.add("active");
+		arr[count].classList.add("carousel__item_active");
 	}
 }
 let obj = new slider(document.getElementById("slider").children);
