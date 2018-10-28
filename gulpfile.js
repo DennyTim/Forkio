25f0c81df7611b5ab47d2eae85227fc72a78c2d3
Tim <luckystudydanit@gmail.com>
2018-10-28 14:01:21 +0300

    28/10/18 Added packages for minify and rename css

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
 'use strict'
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var clean = require('gulp-clean');
-var sourcemaps = require('gulp-sourcemaps');
-var imagemin = require('gulp-imagemin');
+var sass = require('gulp-sass');						//sass
+var browserSync = require('browser-sync').create();		//runtime watcher and changer
+var clean = require('gulp-clean');						//cleaner product directory "dev"
+var cleanCSS = require('gulp-clean-css');				//CSS minifier
+var sourcemaps = require('gulp-sourcemaps');			//SCSS navigation in Chrome inspector
+var imagemin = require('gulp-imagemin');				//Img minify
+var rename = require("gulp-rename");
 
-gulp.task('clean-build', function(){
+gulp.task('clean-dev', function(){
 	return gulp.src('./dev', {read: false})
 		.pipe(clean())
 });
 
-gulp.task('img', ['clean-build'], function(){
+gulp.task('img', ['clean-dev'], function(){
     return gulp.src('./src/img/*.png')
         .pipe(gulp.dest('./dev/img'))
 });
@@ -22,6 +24,10 @@ gulp.task('sass', ['img'], function(){
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(sourcemaps.write())
+		.pipe(cleanCSS({compatibility: 'ie8'}))				// minifyCSS after sourcemaps and sass
+		.pipe(rename(function (path) {						// function of rename extname for .css
+			path.extname = ".min.css";
+		 }))
         .pipe(gulp.dest('./dev/css'))
 });
 
